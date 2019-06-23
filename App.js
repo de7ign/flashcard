1429e894b814f2cad3870bac6c8a8e6c53c92f19
Nihal Murmu <nhlmrm@gmail.com>
2019-06-23 20:10:08 +0530

    Update ui
    add progressbar class, made the card flippable

--- App.js
+++ App.js
@@ -7,38 +7,155 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Dimensions } from "react-native";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Button,
+  Animated,
+  TouchableWithoutFeedback
+} from "react-native";
 
 const { width, height } = Dimensions.get("window");
 
-type Props = {};
-export default class App extends Component<Props> {
+class ProgressBar extends Component {
   render() {
-    const progressBar = (
-      <View style={styles.progressBar}>
-        <View style={styles.progress} />
+    return (
+      <View>
+        <Text style={{ color: "white" }}>{this.props.label}</Text>
+        <View style={styles.progressBar}>
+          <View
+            style={{
+              flex: 1,
+              borderRadius: 10,
+              backgroundColor: this.props.backgroundColor,
+              width: this.props.completed * 100 + "%"
+            }}
+          />
+        </View>
       </View>
     );
+  }
+}
 
+type Props = {};
+export default class App extends Component<Props> {
+  state = {
+    pro: 0.2,
+    learning: 0.5,
+    noob: 0.3,
+    isTouchdisabled: false
+  };
+
+  handlePressCorrect() {}
+
+  handlePressWrong() {}
+
+  componentWillMount() {
+    this.animatedValue = new Animated.Value(0);
+    this.value = 0;
+    this.animatedValue.addListener(({ value }) => {
+      this.value = value;
+    });
+    this.frontInterpolate = this.animatedValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ["0deg", "180deg"]
+    });
+    this.backInterpolate = this.animatedValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ["180deg", "360deg"]
+    });
+  }
+
+  handlePress() {
+    this.setState(prevState => ({
+      isTouchdisabled: !prevState.isTouchdisabled
+    }));
+    if (this.value >= 90) {
+      Animated.spring(this.animatedValue, {
+        toValue: 0,
+        friction: 8,
+        tension: 10
+      }).start();
+    } else {
+      Animated.spring(this.animatedValue, {
+        toValue: 180,
+        friction: 8,
+        tension: 10
+      }).start();
+    }
+  }
+
+  render() {
+    const { pro, learning, noob, isTouchdisabled } = this.state;
+
+    const frontAnimatedStyle = {
+      transform: [{ rotateY: this.frontInterpolate }]
+    };
+
+    const backAnimatedStyle = {
+      transform: [{ rotateY: this.backInterpolate }]
+    };
     return (
       <View style={styles.container}>
-        <View style={styles.card}>
-          <Text>Card</Text>
-        </View>
+        <TouchableWithoutFeedback
+          onPress={() => this.handlePress()}
+          disabled={isTouchdisabled}
+        >
+          <View>
+            <Animated.View style={[frontAnimatedStyle, styles.card]}>
+              <Text>Card</Text>
+            </Animated.View>
+            <Animated.View
+              style={[styles.card, styles.flipCardBack, backAnimatedStyle]}
+            >
+              <Text>back Card</Text>
+              <View
+                style={[{ width: "100%", position: "absolute", bottom: 0 }]}
+              >
+                <TouchableWithoutFeedback onPress={() => this.handlePress()}>
+                  <View
+                    style={[
+                      {
+                        backgroundColor: "rgba(38, 222, 129,0.6)"
+                      },
+                      styles.buttons
+                    ]}
+                  >
+                    <Text style={{ color: "#20bf6b" }}>I got it right</Text>
+                  </View>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={() => this.handlePress()}>
+                  <View
+                    style={[
+                      {
+                        backgroundColor: "rgba(235, 59, 90,0.6)",
+                        borderBottomLeftRadius: 10,
+                        borderBottomRightRadius: 10
+                      },
+                      styles.buttons
+                    ]}
+                  >
+                    <Text style={{ color: "#eb3b5a" }}>I got it wrong</Text>
+                  </View>
+                </TouchableWithoutFeedback>
+              </View>
+            </Animated.View>
+          </View>
+        </TouchableWithoutFeedback>
 
-        <Text>Pro at</Text>
-        {progressBar}
-        <Text>Learning</Text>
-        {progressBar}
-        <Text>Noob at</Text>
-        {progressBar}
+        <ProgressBar label="Pro at" backgroundColor="#26de81" completed={pro} />
+        <ProgressBar
+          label="learning"
+          backgroundColor="#fed330"
+          completed={learning}
+        />
+        <ProgressBar
+          label="noob at"
+          backgroundColor="#eb3b5a"
+          completed={noob}
+        />
       </View>
     );
   }
@@ -47,28 +164,41 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F5FCFF",
+    backgroundColor: "#1565C0",
     padding: width * 0.03
   },
   card: {
-    height: height * 0.3,
-    backgroundColor: "lightblue",
+    minHeight: height * 0.3,
+    height: height * 0.4,
+    backgroundColor: "#FFF",
     borderRadius: 10,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 20
+    marginBottom: 20,
+    backfaceVisibility: "hidden"
   },
   progressBar: {
     height: 20,
-    backgroundColor: "lightblue",
+    backgroundColor: "#FFF",
     borderRadius: 10,
     marginBottom: 10
   },
-  progress: {
-    flex: 1,
-    backgroundColor: "red",
-    width: "50%",
-    overflow: "hidden",
-    borderRadius: 10
+  buttons: {
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  flipCardBack: {
+    backgroundColor: "white",
+    height: height * 0.4,
+    width: width - width * 0.06,
+    position: "absolute",
+    top: 0
+  },
+  flipText: {
+    width: 90,
+    fontSize: 20,
+    color: "black",
+    fontWeight: "bold"
   }
 });
